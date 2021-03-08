75ac119ed6ba8cbed181a827b7c77dbc527ec1c6
Miguel <miguel.moreno.baladron@gmail.com>
2021-03-08 16:53:13 +0000

    Improve efficiency by checking even numbers. Return true with 2

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const isPrime = (num) => {
+    if (num === 2) {
+        return true;
+    }
+
+    if (num % 2 === 0) {
+        return false;
+    }
+
     const upper = Math.floor(Math.sqrt(num));
+    
     for (let i = upper; i > 1; i--) {
         if (num % i === 0) {
             return false;
