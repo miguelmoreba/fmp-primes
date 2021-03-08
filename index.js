43d746c7d6cf75c77da0edfb4b0832fcff9581b9
Miguel <miguel.moreno.baladron@gmail.com>
2021-03-08 17:14:33 +0000

    Add space

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const isPrime = (num) => {
     }
 
     const upper = Math.floor(Math.sqrt(num));
-    
+
     for (let i = upper; i > 1; i--) {
         if (num % i === 0) {
             return false;
