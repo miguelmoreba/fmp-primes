d8839b5e7b3c30c0f7ae02a5107e994cbc44e383
Miguel <miguel.moreno.baladron@gmail.com>
2021-03-09 22:35:21 +0000

    Add performance tests

---
+++ test/performance.test.js
@@ -0,0 +1,13 @@
+const { getMatrix } = require('../index');
+
+describe("getMatrix()", function() {
+    it ("should take less than 10 seconds to get matrix for 3000 records", function() {
+        this.timeout(10000);
+        getMatrix(3000);
+    });
+
+    it("should take less than 20 seconds to get matrix for 5000 records", function() {
+        this.timeout(20000);
+        getMatrix(5000);
+    })
+});
