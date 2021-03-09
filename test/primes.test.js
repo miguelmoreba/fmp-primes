d76111faf3667f7a6b80caaa92162fece6ff314d
Miguel <miguel.moreno.baladron@gmail.com>
2021-03-09 22:23:19 +0000

    Substitute jest with mocha and refactor tests accordingly

--- test/primes.test.js
+++ test/primes.test.js
@@ -1,48 +1,53 @@
-const { isPrime, generatePrimeArray } = require('../index');
+const { isPrime, generatePrimeArray, getMatrix } = require('../index');
+const expect = require("chai").expect;
 
-test('isPrime() returns true when first 15 prime numbers are passed', () => {
-    expect(isPrime(2)).toBe(true);
-    expect(isPrime(3)).toBe(true);
-    expect(isPrime(5)).toBe(true);
-    expect(isPrime(7)).toBe(true);
-    expect(isPrime(11)).toBe(true);
-    expect(isPrime(13)).toBe(true);
-    expect(isPrime(17)).toBe(true);
-    expect(isPrime(19)).toBe(true);
-    expect(isPrime(23)).toBe(true);
-    expect(isPrime(29)).toBe(true);
-    expect(isPrime(31)).toBe(true);
-    expect(isPrime(37)).toBe(true);
-    expect(isPrime(41)).toBe(true);
-    expect(isPrime(43)).toBe(true);
-    expect(isPrime(47)).toBe(true);
+describe("isPrime()", function () {
+    it('returns true when first 15 prime numbers are passed', function () {
+        expect(isPrime(2)).to.be.true;
+        expect(isPrime(3)).to.be.true;
+        expect(isPrime(5)).to.be.true;
+        expect(isPrime(7)).to.be.true;
+        expect(isPrime(11)).to.be.true;
+        expect(isPrime(13)).to.be.true;
+        expect(isPrime(17)).to.be.true;
+        expect(isPrime(19)).to.be.true;
+        expect(isPrime(23)).to.be.true;
+        expect(isPrime(29)).to.be.true;
+        expect(isPrime(31)).to.be.true;
+        expect(isPrime(37)).to.be.true;
+        expect(isPrime(41)).to.be.true;
+        expect(isPrime(43)).to.be.true;
+        expect(isPrime(47)).to.be.true;
+    });
+
+    it('returns false when first non prime numbers are passed', function () {
+        expect(isPrime(4)).to.be.false;
+        expect(isPrime(6)).to.be.false;
+        expect(isPrime(8)).to.be.false;
+        expect(isPrime(9)).to.be.false;
+        expect(isPrime(10)).to.be.false;
+        expect(isPrime(12)).to.be.false;
+        expect(isPrime(14)).to.be.false;
+        expect(isPrime(15)).to.be.false;
+        expect(isPrime(16)).to.be.false;
+        expect(isPrime(18)).to.be.false;
+        expect(isPrime(20)).to.be.false;
+        expect(isPrime(21)).to.be.false;
+        expect(isPrime(22)).to.be.false;
+        expect(isPrime(24)).to.be.false;
+        expect(isPrime(25)).to.be.false;
+    });
 });
 
-test('isPrime() returns false when first 15 non prime numbers are passed', () => {
-    expect(isPrime(4)).toBe(false);
-    expect(isPrime(6)).toBe(false);
-    expect(isPrime(8)).toBe(false);
-    expect(isPrime(9)).toBe(false);
-    expect(isPrime(10)).toBe(false);
-    expect(isPrime(12)).toBe(false);
-    expect(isPrime(14)).toBe(false);
-    expect(isPrime(15)).toBe(false);
-    expect(isPrime(16)).toBe(false);
-    expect(isPrime(18)).toBe(false);
-    expect(isPrime(20)).toBe(false);
-    expect(isPrime(21)).toBe(false);
-    expect(isPrime(22)).toBe(false);
-    expect(isPrime(24)).toBe(false);
-    expect(isPrime(25)).toBe(false);
+describe("generatePrimeArray()", function () {
+    it("returns correct 3 first primes", function () {
+        expect(generatePrimeArray(3)).to.deep.equal([2, 3, 5]);
+    });
 });
 
-test('generatePrimeArray() returns correct array for 3 numbers', () => {
-    const expected = [2, 3, 5];
-    expect(generatePrimeArray(3).toString()).toMatch(expected.toString());
-});
-
-test('getMatrix() returns correct result for 3 numbers', () => {
-    const expected = "|  | 2| 3| 5|\n| 2| 4| 6| 10|\n| 3| 6| 9| 15|\n| 5| 10| 15| 25|"
-
-    expect(getMatrix(3)).toMatch(expected);
+describe("getMatrix()", function () {
+    it("returns formatted matrix string", function () {
+        const expected = "|  | 2| 3| 5|\n| 2| 4| 6| 10|\n| 3| 6| 9| 15|\n| 5| 10| 15| 25|\n"
+        expect(getMatrix(3)).to.equal(expected);
+    });
 });
